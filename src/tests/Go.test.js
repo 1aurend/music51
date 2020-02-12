4cf8bcdd8167caa7cf88ba163ed2d3445b58a8bd
Lauren Davidson <ldavidson@g.harvard.edu>
2020-02-11 20:39:40 -0500

    Add tests for Quiz

---
+++ src/tests/Go.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Go from '../components/views/buttons/Go'
+
+
+test('calls "generator" on click', () => {
+  const generateQuiz = jest.fn(() => 'launching generator')
+  const { asFragment, getByRole } = render(
+  <Go onClick={generateQuiz}/>
+  )
+  fireEvent.click(getByRole('button'))
+  expect(generateQuiz).toHaveBeenCalledTimes(1)
+  expect(generateQuiz).toHaveReturnedWith('launching generator')
+  expect(asFragment()).toMatchSnapshot()
+})
