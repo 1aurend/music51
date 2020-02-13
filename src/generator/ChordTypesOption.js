25bc58a66d9a6bae9378fd4b66f57c0384dc4185
James Bean <jbean46@gmail.com>
2020-02-12 17:06:05 -0800

    Add TODO comment over ChordTypesOption

--- src/generator/ChordTypesOption.js
+++ src/generator/ChordTypesOption.js
@@ -1,3 +1,5 @@
+// TODO: Discuss how to flesh this enumeration out when we expose all of the
+// different chord types to the user for constraining their session.
 export const ChordTypesOption = {
   TRIADS: "triads",
   SEVENTHS: "sevenths",
