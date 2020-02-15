cc3f8aa428cdff13e651f20811fa9054ed259907
James Bean <jbean46@gmail.com>
2020-02-15 15:12:38 -0800

    Destructure chord context, build up skeleton

--- src/generator/chordGenerator.js
+++ src/generator/chordGenerator.js
@@ -31,7 +31,130 @@ const RootOption = {
  * @returns             An array of questions (and answers) for the given `chordContext`.
  */
 export function questions(chordContext) {
-  // TODO
+
+  const rootLetter = chordContext.chordDescription.root.letter
+  const rootAccidental = chordContext.chordDescription.root.accidental
+  const inversion = chordContext.chordDescription.inversion
+  const degree = chordContext.romanNumeralContext.degree
+  const roman = chordContext.romanNumeralContext.romanNumeral
+  const key = chordContext.key
+  const chordType = chordContext.chordType
+
+  // FIXME: Infer `romanQuality` somehow?
+  const romanQuality = "???"
+  // FIXME: Infer `inversionQuality` somehow?
+  const inversionQuality = "???"
+
+  let romanInversionOptions
+
+  // [roman + inversionQuality + " " + inversion]
+
+  if (chordType === 'triad') {
+    romanInversionOptions = [
+      roman + inversionQuality,
+      roman + inversionQuality + '63',
+      roman + inversionQuality + '64'
+    ]
+  } else if (chordType === 'seventh') {
+    romanInversionOptions = [
+      roman + inversionQuality,
+      roman + inversionQuality + '65',
+      roman + inversionQuality + '43',
+      roman + inversionQuality + '42'
+    ]
+  }
+
+  // TODO:
+  // // aggregate options for chord quality question
+  // let qualityOptions = []
+  // Object.keys(template).map(type => {qualityOptions.push(rootLetter + rootAccidental + type)})
+
+  // // aggregate options for inversions question
+  // let inversionOptions = []
+  // inversions.map(type => {inversionOptions.push(rootLetter + rootAccidental + newStructure + " " + type)})
+
+  // // push notes into questions before adjusting accidentals for key sig
+  // chord.questions[0].answers.push(noteLetter);
+
+
+  // // FIXME: This should be its own function
+  // // only show natural in question choices if it's an alteration from the key sig
+  // if(accidental != '♮'){
+  //   chord.questions[1].choices.push(noteLetter+accidental);
+  // }
+  // else if ((accidental === '♮') && (keySignatures[keySignature].notes[keySignatures[keySignature].notes.findIndex(function(syllable){return syllable.refIP === noteSyllable})].accidental != '♮')){
+  //   chord.questions[1].choices.push(noteLetter+'♮');
+  // }
+  // else {
+  //   chord.questions[1].choices.push(noteLetter);
+  // }
+
+  // // adjust accidentals for key sig (if an accidental is in the key sig, don't add it to the note)
+  // if(accidental === keySignatures[keySignature].notes[keySignatures[keySignature].notes.findIndex(function(syllable){return syllable.refIP === noteSyllable})].accidental){
+  //   accidental = "";
+  // }
+
+  console.log("rootLetter: " + JSON.stringify(rootLetter))
+
+  
+
+  // Consider breaking this out to a factory-type function, like:
+  // question(chordContext, type)
+  let skeleton = [
+    {
+      "type": "Names",
+      "questionText": "Name the letter positions from lowest to highest.",
+      "answers": [], // will populate in the loop
+      "ordered": true,
+      "choices": [
+          "A",
+          "B",
+          "C",
+          "D",
+          "E",
+          "F",
+          "G"
+      ]
+    },
+    {
+      "type": "Roots",
+      "questionText": "What's the root note?",
+      "answers": [rootLetter+rootAccidental],
+      "choices": [] // will populate in the loop
+    },
+    {
+      "type": "Degrees",
+      "questionText": "In a " + key + " key, what degree is this chord built on?",
+      "answers": [degree + "^"],
+      "choices": [
+          "1^",
+          "2^",
+          "3^",
+          "4^",
+          "5^",
+          "6^",
+          "7^"
+      ]
+    },
+    // {
+    //   "type": "Quality",
+    //   "questionText": "What's the chord's quality?",
+    //   "answers": [rootLetter + rootAccidental + newStructure],
+    //   "choices": qualityOptions
+    // },
+    // {
+    //   "type": "Numerals",
+    //   "questionText": "Which roman numeral describes this chord’s degree and quality?",
+    //   "answers": [roman + romanQuality],
+    //   "choices": romanOptions
+    // },
+    {
+      "type": "Inversions",
+      "questionText": "What's the inversion?",
+      "answers": [roman + inversionQuality + inversion],
+      "choices": romanInversionOptions
+    }
+  ]
   return []
 }
 
@@ -369,6 +492,7 @@ export function randomChordContext(options) {
   const result = {
     clef: clef,
     keySignature: vexFlowKeySignature,
+    chordType: chordType,
     chordDescription,
     romanNumeralContext,
     notes: staffAdjustedNotes
