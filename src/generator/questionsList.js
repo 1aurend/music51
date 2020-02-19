67e688dca858e5ce2c9563ec9986d3217c1c581d
Lauren Davidson <ldavidson@g.harvard.edu>
2020-02-18 22:31:45 -0500

    Update tallyMeans and getChartData to accomodate variety in questions and add tests

---
+++ src/generator/questionsList.js
@@ -0,0 +1,52 @@
+// NOTE: this is ld's prototyping version of this. check for errors before merging into anything real!
+
+export const questionsList = {
+  LETTER_NAMES: {
+    fullName: 'Letter Positions',
+    fullText: 'Name the letter positions from lowest to highest.',
+    shortName: 'Notes',
+    abbrev: 'LP'
+  },
+  ROOT: {
+    fullName: 'Root Note',
+    fullText: 'What\'s the root note?',
+    shortName: 'Root', // shortName is for the stats display, so it's describing all ROOT questions in the round. So should shortName be plural, even if the question name is singular (all the Roots you've defined this round)? A question like LETTER_POSITIONS is plural because the question itself asks for multiple letter positions. Or should they match-- "Role" describes all the Roles you have defined in all the ROLE questions. For now, leaving singular/pural to match the question name.
+    abbrev: 'RN'
+  },
+  DEGREE: {
+    fullName: 'Degree',
+    fullText: 'In a key, what degree is this chord built on?', // key == 'Major' or 'minor'
+    shortName: 'Degree',
+    abbrev: 'Deg'
+  },
+    ROLE: {
+    fullName: 'Role',
+    fullText: 'What is this chord\'s relationship to the key?',
+    shortName: 'Role', // Grouping? Category?
+    abbrev: 'R' // Cat? Grp? RRG.
+  },
+    NUMERAL: {
+    fullName: 'Numeral',
+    fullText: 'Which roman numeral describes this chord\'s degree and quality?',
+    shortName: 'Numeral',
+    abbrev: 'Num'
+  },
+    QUALITY: {
+    fullName: '',
+    fullText: 'What type of grouping is it?', // grouping == 'SEVENTH', 'CHROMATIC_VARIATION' etc.
+    shortName: 'Chord',
+    abbrev: 'Qlt'
+  },
+    INVERSION: {
+    fullName: 'Inversion',
+    fullText: 'What\'s the inversion?',
+    shortName: 'Inversion',
+    abbrev: 'Inv'
+  },
+    FOLLOWED_BY: {
+    fullName: 'Followed By',
+    fullText: 'Which chord is most likely to follow this chord?',
+    shortName: 'Follow',
+    abbrev: 'Fol' // This one isn't great
+  }
+}
--- src/tests/tally.test.js
+++ src/tests/tally.test.js
@@ -1,4 +1,4 @@
-import { listAttemptsByQuestionType, listTimesByQuestionType, tallyRound } from '../components/data/tallyMeans'
+import { listAttemptsByQuestionType, listTimesByQuestionType, tallyRound, tallyMeans } from '../components/data/tallyMeans'
 
 test('listAttemptsByQuestionType returns a single array', () => {
   const data = [
@@ -40,6 +40,49 @@ test('listAttemptsByQuestionType returns a single array', () => {
   const attempts = listAttemptsByQuestionType(data, 'names')
   console.log(attempts)
   expect(attempts.length).toBeGreaterThan(1)
+})
+
+test('listAttemptsByQuestionType returns when type does not exist', () => {
+  const data = [
+    {
+      questions: [
+        {
+          type: 'names',
+          answers: [{
+            tries: [1, 2],
+            elapsedTime: 1.2
+          }]
+        },
+        {
+          type: 'degree',
+          answers: [{
+            tries: [1, 2, 3]
+          }]
+        },
+      ]
+    },
+    {
+      questions: [
+        {
+          type: 'test',
+          answers: [{
+            tries: [1, 2, 3],
+            elapsedTime: 2.3
+          }]
+        },
+        {
+          type: 'degree',
+          answers: [{
+            tries: [1, 2, 3, 4]
+          }]
+        },
+      ]
+    },
+  ]
+  const attempts = listAttemptsByQuestionType(data, 'test')
+  console.log(`missing question attempts: ${attempts}`)
+  console.log(attempts)
+  expect(attempts.length).toBe(1)
 })
 
 test('listTimesByQuestionType returns a single array', () => {
@@ -128,4 +171,126 @@ test('tallyRound returns an object', () => {
   const means = tallyRound(data)
   console.log(means)
   expect(means).toBeTruthy()
+})
+
+test('tallyMeans returns with nulls for unused questions', () => {
+  const data = [
+    {
+      questions: [
+        {
+          type: 'ROLE',
+          answers: [{
+            tries: [1, 2],
+            elapsedTime: 1.2
+          }]
+        },
+        {
+          type: 'LETTER_NAMES',
+          answers: [{
+            tries: [1, 2, 3],
+            elapsedTime: 1.4
+          }]
+        },
+      ]
+    },
+    {
+      questions: [
+        {
+          type: 'LETTER_NAMES',
+          answers: [{
+            tries: [1, 2, 3],
+            elapsedTime: 2.3
+          }]
+        },
+        {
+          type: 'QUALITY',
+          answers: [{
+            tries: [1, 2, 3, 4],
+            elapsedTime: 0.5
+          }]
+        },
+      ]
+    },
+  ]
+  const state = {
+         "tally": {
+            "LETTER_NAMES": {
+               "attempts": [
+                  3
+               ],
+               "times": [
+                  2.3
+               ]
+            },
+            "ROOT": {
+               "attempts": [
+                  null
+               ],
+               "times": [
+                  null
+               ]
+            },
+            "DEGREE": {
+               "attempts": [
+                  4
+               ],
+               "times": [
+                  4.5
+               ]
+            },
+            "ROLE": {
+               "attempts": [
+                  2
+               ],
+               "times": [
+                  1.2
+               ]
+            },
+            "NUMERAL": {
+               "attempts": [
+                  null
+               ],
+               "times": [
+                  null
+               ]
+            },
+            "QUALITY": {
+               "attempts": [
+                  null
+               ],
+               "times": [
+                  null
+               ]
+            },
+            "INVERSION": {
+               "attempts": [
+                  2
+               ],
+               "times": [
+                  0.3
+               ]
+            },
+            "FOLLOWED_BY": {
+               "attempts": [
+                  null
+               ],
+               "times": [
+                  null
+               ]
+            }
+         },
+         "questionTypes": [
+            "ROLE",
+            "LETTER_NAMES",
+            "DEGREE",
+            "Overall"
+         ]
+      }
+  const means = tallyMeans(state, data)
+  console.log(JSON.stringify(means, null, 3))
+  expect(means).toBeTruthy()
+  const letterNames = means.tally.LETTER_NAMES.attempts
+  expect(letterNames).toEqual([3,3])
+  const quality = means.tally.QUALITY.times
+  expect(quality).toEqual([null,0.5])
 })
