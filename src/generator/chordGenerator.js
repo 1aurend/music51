5cda019bfc4a65da75a1f5ebb19ffc27a8def035
James Bean <jbean46@gmail.com>
2020-02-25 12:30:50 -0800

    Clean up

--- src/generator/chordGenerator.js
+++ src/generator/chordGenerator.js
@@ -76,10 +76,8 @@ export function randomChordContext(options) {
   const keySignature = chooseKeySignature()
   // Choose a random `ChordType` from the constraints provided by the user
   const chordType = chooseChordType()
-  console.log("Chord Type: " + JSON.stringify(chordType))
   // Choose a random `ChordStructure` belonging to the chosen `ChordType` family
   const chordStructure = chooseChordStructure(chordType)
-  console.log("Chord Structure: " + chordStructure.displayName)
   // Choose a random inversion from those afforded by the chosen `ChordStructure`
   const inversion = chooseInversion(chordStructure)
   // Choose whether we will be in a major or minor mode.
@@ -142,7 +140,6 @@ export function makeChordDescription(
     keySignature,
     romanNumeralContext
   )
-  console.log("ROOT: " + concretizedRoot.letter + concretizedRoot.accidental)
   return {
     root: concretizedRoot,
     structure: chordStructure,
